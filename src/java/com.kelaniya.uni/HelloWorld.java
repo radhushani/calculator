1e3169d82c477ed6bd499eb4a3d2cbfd05600d83
Radhushani <radushani111@gmail.com>
2021-08-05 13:30:04 +0530

    Add calculator feature to helloworld file

--- src/java/com.kelaniya.uni/HelloWorld.java
+++ src/java/com.kelaniya.uni/HelloWorld.java
@@ -1,8 +1,26 @@
 package com.kelaniya.uni;
+import java.io.*;
+import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello world");
-    }
+    public static void main(String[] args) throws FileNotFoundException {
 
+        System.out.println("Enter mathematical expression(Add :'+',Sub: '-',Mul:'*'):");
+        Scanner scanner = new Scanner(System.in);
+        String symbol = scanner.next();
+
+        File file = new File("src/java/com.kelaniya.uni/number.txt");
+        Scanner scan = new Scanner(file);
+        String num1 =scan.next();
+        String num2 =scan.next();
+
+
+        System.out.println("Answer for your mathematical expression:");
+        switch (symbol) {
+            case "+" -> System.out.println(Integer.parseInt(num1) + Integer.parseInt(num2));
+            case "-" -> System.out.println(Integer.parseInt(num1) - Integer.parseInt(num2));
+            case "*" -> System.out.println(Integer.parseInt(num1) * Integer.parseInt(num2));
+            default -> System.out.println("enter a correct symbol");
+        }
+    }
 }
