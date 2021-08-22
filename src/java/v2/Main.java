a34d321f41098824d76e8f73b1d5e11c5bef2a0c
Radhushani <radushani111@gmail.com>
2021-08-22 21:21:08 +0530

    V2 -final code

--- src/java/v2/Main.java
+++ src/java/v2/Main.java
@@ -1,19 +1,49 @@
 package v2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-        if(args.length==0){
+import static java.lang.Double.parseDouble;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //Validation part
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
 
-        String operator =args[0];
+        String operator = args[0];
 
-        if(!(operator.equals("add")||operator.equals("div")||operator.equals("mul"))){
+        if (!(operator.equals("add") || operator.equals("div") || operator.equals("mul"))) {
             System.out.println("Please provide add,sub or mul as the operator argument.");
         }
-        System.out.println(args[0]);
+
+        //read the numbers text file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("C:\\Users\\radhushani\\Documents\\Software_construction_project\\calculator\\numbers.txt")
+        );
+
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        // Or we can use switch case
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        }
+
+        System.out.println("The result is " + result);//v2 - final code
+
     }
 
 }
